f36746b56b72ee0e57a7b29e60ec88b0c975fb12
Siebter <30348282+Siebter@users.noreply.github.com>
2017-07-21 18:20:26 +0200

    Update getartistinfos.js

--- getartistinfos.js
+++ getartistinfos.js
@@ -1,11 +1,9 @@
-
-
 $(document).on('change', 'select', function () {
 
     var country = $(this).val();
-    limit = 6;
-    apiKey = 'e9ea677a6aeed784c1b9fa2919e17347';
-    format = 'json';
+    var limit = 6;
+    var apiKey = 'e9ea677a6aeed784c1b9fa2919e17347';
+    var format = 'json';
     var args = '';
     args += 'country=' + country;
     args += '&limit=' + limit;
@@ -20,16 +18,12 @@ function showTopArtists(lastfmData) {
     $('.resultbox p').remove();
     $('.resultbox img').remove();
     $.each(lastfmData.topartists.artist, function (idx, art) {
-   
+//     
         var artname = art.name;
         var image = art.image[2]["#text"];
 
-        $(".resultbox").append('<a href="https://de.wikipedia.org/wiki/' + artname + '"' + ' target="_blank"><img src="' + image + '"></a>');
+        $(".resultbox").append('<a href="https://de.wikipedia.org/wiki/' + artname + '" target="_blank"><img src="' + image + '"></a>');
     });
     
     $(".smallText").css("visibility", "visible");
-}
-;
-
-
-
+};
